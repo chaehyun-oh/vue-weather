07ea2cca9d7cba1359eb822091d6777fdc6023eb
chaehyun_oh <ohchaehh@gmail.com>
2022-07-07 21:35:08 +0900

    vue.js practice weather project v.1 update

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<HeaderDisplay />
+	<WeatherContents />
+</template>
+
+<script>
+import HeaderDisplay from "./components/HeaderDisplay";
+import WeatherContents from "./components/weather/WeatherContent.vue";
+
+export default {
+	name: "App",
+	components: {
+		HeaderDisplay,
+		WeatherContents,
+	},
+};
+</script>
+
+<style></style>
